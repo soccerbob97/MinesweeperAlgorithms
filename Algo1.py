222b35a19532d664247fb4e15fb5f0e46b7c360d
Armaan Lala <armaanlala@gmail.com>
2021-04-11 16:18:30 -0400

    Find initial zeros

--- Algo1.py
+++ Algo1.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 
+# initial = json.loads(open('deterministic_board.json').read())
 initial = json.loads(open('test_board.json').read())
 
 height,width = initial["dim"].split(",")
@@ -14,6 +15,24 @@ visibleBoard =  np.empty(shape=(height,width))
 visibleBoard.fill(-1)
 visibleBoard = visibleBoard.astype(int)
 
+#temp variable
 finalBoard = [[int(answerArray[i*width + j]) for i in range(height)] for j in range(width)]
-answerArray = np.array(finalBoard)
+answerKey = np.array(finalBoard)
 
+def visitSquare(x: int,  y: int):
+    if (x < 0 or y < 0 or x >= width or y>= height):
+        return
+    if visibleBoard[x][y] == -1:
+        visibleBoard[x][y] = answerKey[x][y]
+        if (visibleBoard[x][y] == 0):
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    try:
+                        visitSquare(x+i,y+j)
+                    except:
+                        return
+        elif (visibleBoard[x][y] == 9):
+            print("bomb")
+
+visitSquare(int(safeSquare[0]),int(safeSquare[2]))
+print(visibleBoard)
