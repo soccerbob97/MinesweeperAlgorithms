9b54161a9ff267b530a0268220b8675c2c248e7f
Armaan Lala <armaanlala@gmail.com>
2021-04-19 21:14:45 -0400

    about to nuke

--- Algo1.py
+++ Algo1.py
@@ -2,7 +2,7 @@ import json
 import numpy as np
 
 # initial = json.loads(open('deterministic_board.json').read())
-initial = json.loads(open('test_board.json').read())
+initial = json.loads(open('test_boards/varied_size/10_10_10.json').read())
 
 height,width = initial["dim"].split(",")
 height = int(height)
@@ -10,29 +10,31 @@ width = int(width)
 numBombs = initial["bombs"]
 safeSquare = initial["safe"]
 answerArray = initial["board"]
+print(safeSquare)
 
 visibleBoard =  np.empty(shape=(height,width))
 visibleBoard.fill(-1)
 visibleBoard = visibleBoard.astype(int)
 
 #temp variable
-finalBoard = [[int(answerArray[i*width + j]) for i in range(height)] for j in range(width)]
+finalBoard = [[int(answerArray[j*width + i]) for i in range(width)] for j in range(height)]
 answerKey = np.array(finalBoard)
 
 def visitSquare(x: int,  y: int):
+    # x = width - x
+    # y =  height - y
     if (x < 0 or y < 0 or x >= width or y>= height):
         return
     if visibleBoard[x][y] == -1:
-        visibleBoard[x][y] = answerKey[x][y]
-        if (visibleBoard[x][y] == 0):
-            for i in range(-1,2):
-                for j in range(-1,2):
-                    try:
-                        visitSquare(x+i,y+j)
-                    except:
-                        return
-        elif (visibleBoard[x][y] == 9):
-            print("bomb")
+    #     visibleBoard[x][y] = finalBoard[x][y]
+        visibleBoard[x,y] = answerKey[x,y]
+        # visibleBoard[x,y]  = int(answerArray[y*width + x])
+        print(visibleBoard[x,y])
+
+
+
 
 visitSquare(int(safeSquare[0]),int(safeSquare[2]))
-print(visibleBoard)
+
+
+print(answerKey)
