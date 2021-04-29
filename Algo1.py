a822534be9cef978080fa7ad7cab25a06cf064f2
Armaan Lala <armaanlala@gmail.com>
2021-04-29 11:29:38 -0400

    Refactor

--- Algo1.py
+++ Algo1.py
@@ -5,6 +5,40 @@ from numpy import unravel_index
 import os
 
 
+def visitSquare(row: int,  col: int):
+    if (row < 0 or col < 0 or row >= height or col>= width):
+        return
+    if visibleBoard[row][col] == -1:
+        coords.append((row,col))
+        visibleBoard[row,col]  = int(answerArray[row*width + col])
+
+        probabilityBoard[row,col] = -9
+        updateProbability(row,col)
+
+        if(visibleBoard[row,col]  == 9):
+            global numBombs
+            numBombs+=1
+
+
+def updateProbability(row: int, col:int): 
+    for i in range(-1,2):
+        for j in range(-1,2):
+            if (row+i,col+j) not in coords and not (row + i < 0 or col + j < 0 or row + i >= height or col + j>= width):
+                if visibleBoard[row,col] == 0:
+                    probabilityBoard[row + i, col + j] = -100
+                elif not visibleBoard[row,col] == 9:
+                    probabilityBoard[row + i, col + j] += visibleBoard[row,col]
+                elif visibleBoard[row,col] == 9:
+                    probabilityBoard[row + i, col + j] -= 2
+
+
+def distanceToCenter(row:int , col:int) -> int:
+    val = (height/2 - row)**2 + (width /2 - col)**2
+    return math.sqrt(val)
+
+
+
+
 stats_total = 0
 stats_mined = 0
 stats_bombs = 0
@@ -17,7 +51,7 @@ for filename in os.listdir(path):
 
         initial = json.loads(open("varied_size_boards/" + filename).read())
         
-
+        # Initialize variables
         height,width = initial["dim"].split(",")
         height = int(height)
         width = int(width)
@@ -25,72 +59,35 @@ for filename in os.listdir(path):
         safeSquare = initial["safe"]
         answerArray = initial["board"]
 
+        # Initialize the board visible to the computer
         visibleBoard =  np.empty(shape=(height,width))
         probabilityBoard =  np.empty(shape=(height,width))
         visibleBoard.fill(-1)
         visibleBoard = visibleBoard.astype(int)
 
+        # Initialize the probability board for decisions
         probabilityBoard =  np.empty(shape=(height,width))
         probabilityBoard.fill(0)
         probabilityBoard = probabilityBoard.astype(int)
-        #temp variable
-        finalBoard = [[int(answerArray[j*width + i]) for i in range(width)] for j in range(height)]
+        finalBoard = [[int(answerArray[row*width + col]) for col in range(width)] for row in range(height)]
         answerKey = np.array(finalBoard)
-
-        max = 0
-        for row in answerKey:
-            for val in row:
-                if val > max and val < 9:
-                    max = val
-        # print(max)
-
 
         coords = []
         numBombs = 0
-
-        def visitSquare(x: int,  y: int):
-            
-            if (x < 0 or y < 0 or x >= height or y>= width):
-                return
-            if visibleBoard[x][y] == -1:
-                coords.append((x,y))
-                visibleBoard[x,y]  = int(answerArray[x*width + y])
-
-                probabilityBoard[x,y] = -9
-                updateProbability(x,y)
-
-                if(visibleBoard[x,y]  == 9):
-                    global numBombs
-                    numBombs+=1
-                # print(visibleBoard[x,y])
-
-
-        def updateProbability(x: int, y:int): 
-            
-            for i in range(-1,2):
-                for j in range(-1,2):
-                    if (x+i,y+j) not in coords and not (x + i < 0 or y + j < 0 or x + i >= height or y + j>= width):
-                        if visibleBoard[x,y] == 0:
-                            probabilityBoard[x + i, y + j] = -100
-                        elif not visibleBoard[x,y] == 9:
-                            probabilityBoard[x + i, y + j] += visibleBoard[x,y]
-                        elif visibleBoard[x,y] == 9:
-                            probabilityBoard[x + i, y + j] -= 2
-
-        def distanceToCenter(x:int , y:int) -> int:
-            val = (height/2 - x)**2 + (width /2 - y)**2
-            return math.sqrt(val)
 
         visitSquare(int(safeSquare.split(",")[0]),int(safeSquare.split(",")[1]))
 
         maxCoords = []
 
         mined = 0
+
+        # While not all bombs have been found
         while(str(numBombs) != str(maxNumBombs)):
-        # for i in range(3):
             minDistance = height + width
             xMax,yMax = np.where(probabilityBoard == np.max(probabilityBoard))
 
+            # MinCoord is the square with the highest probability 
+            # and if there is a tie, it is the one closest to the center
             minCoord = ()
             for j in range(len(xMax)):
                 if distanceToCenter(xMax[j],yMax[j]) < minDistance:
